54d1242f967fe20c4fc1933bcc67c0ec65643ad0
Roshan Singh <roshansingh6039@gmail.com>
2024-08-05 00:05:05 +0530

    fixes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const cors = require('cors');
 
 // middleware
 
@@ -17,6 +18,10 @@ app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
+app.use(cors({
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200,  // Some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 const port = process.env.PORT || 5000;
 
 const start = async () => {
